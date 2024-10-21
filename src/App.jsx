6422f35907b9836c84d7fe47c696d4f199e6ea52
“Debajyoti <debajyotipandit8@gmail.com>
2024-10-22 03:17:52 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="font-audiowide font-bold text-3xl">
+        hi there dave here
+      </div>
+      <div>hi there priya here</div>
+      <div>lorem1200</div>
+    </>
+  );
+}
+
+export default App;
